633663c61ebf95bc4db8bf503842adbd7c00a601
David Seidenberg <davidseidenberg@gmail.com>
2018-03-05 19:30:55 -0800

    Start building proxy app

--- client/index.js
+++ client/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
+const title = 'FossaBnB';
+
+const Components = ({myID}) => {
+  <div className = "my-components">
+    <Photos />
+  </div>
+}
 
 ReactDOM.render(
   <div>{title}</div>,
